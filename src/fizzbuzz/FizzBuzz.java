3da8c477dd8f7bad34584aea6ac8bed2318da697
Daoud LAMALMI <102736413+MadokaIII@users.noreply.github.com>
2023-04-20 17:10:32 +0200

    TP fini

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -2,17 +2,20 @@ package fizzbuzz;
 
 public class FizzBuzz {
 
-    private static final String UN = "1";
-    private static final String DEUX = "2";
+    public static final int[] cases = { 3, 5 };
+    public static final String[] strings = { "Fizz", "Buzz" };
 
     private FizzBuzz() {
         throw new IllegalStateException("Utility class");
     }
 
-    public static String fizzbuzz(int i) {
-        if (i == 1) return UN;
-        if (i == 2) return DEUX;
-        return "";
+    public static String fizzbuzz(int number) {
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < cases.length; i++) {
+            if (number % cases[i] == 0) str.append(strings[i]);
+        }
+        if (str.isEmpty()) str.append(number);
+        return str.toString();
     }
 
 }
--- test/fizzbuzz/FizzBuzzTest.java
+++ test/fizzbuzz/FizzBuzzTest.java
@@ -7,12 +7,28 @@ import static org.junit.jupiter.api.Assertions.*;
 class FizzBuzzTest {
 
     @Test
-    void answer1() {
+    void answerInt() {
         assertEquals("1", FizzBuzz.fizzbuzz(1));
+        assertEquals("2", FizzBuzz.fizzbuzz(2));
+        assertEquals("4", FizzBuzz.fizzbuzz(4));
     }
 
     @Test
-    void answer2() {
-        assertEquals("2", FizzBuzz.fizzbuzz(2));
+    void answerFizz() {
+        assertEquals("Fizz", FizzBuzz.fizzbuzz(3));
+        assertEquals("Fizz", FizzBuzz.fizzbuzz(6));
     }
+
+    @Test
+    void answerBuzz() {
+        assertEquals("Buzz", FizzBuzz.fizzbuzz(5));
+        assertEquals("Buzz", FizzBuzz.fizzbuzz(10));
+    }
+
+    @Test
+    void answerFizzBuzz(){
+        assertEquals("FizzBuzz", FizzBuzz.fizzbuzz(15));
+        assertEquals("FizzBuzz", FizzBuzz.fizzbuzz(30));
+    }
+
 }

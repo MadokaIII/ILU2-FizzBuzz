15cce4a1e7e133509f3e9edb8f92a388857f841e
Daoud LAMALMI <102736413+MadokaIII@users.noreply.github.com>
2023-04-20 16:28:09 +0200

    Initial Commit

---
+++ test/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,14 @@
+package fizzbuzz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FizzBuzzTest {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
